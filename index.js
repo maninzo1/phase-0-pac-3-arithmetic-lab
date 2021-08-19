5f014714655eefcc8fb1c0803b6f174a1ba2330f
MaryAninzo <aninzom211@gmail.com>
2021-08-19 10:47:46 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add( ) {} 
+function subtract( ) {} 
+function multiply( ) {} 
+function divide( ) {} 
+
+function add(a,b) {
+    return a + b;
+}
+console.log(add(a,b)) ;  
+
+function subtract(a,b) {
+    return a - b;
+}
+console.log(subtract(a,b)) ; 
+
+function multiply(a,b) {
+    return a * b;
+}
+console.log(multiply(a,b)) ; 
+
+function divide(a,b) {
+    return a / b;
+}
+console.log(divide(a,b)) ; 
+
+function increment(a) {
+    return a + 1 ;
+}
+function decrement(a) {
+    return a - 1 ;
+}
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a, 10);
+}
